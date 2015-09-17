026775402c060162495a04e1549b75653d1fefc1
Peter G. Marczis <peter.marczis@rovio.com>
2015-09-17 11:55:29 +0300

    Added comments into the default config file

--- WebChecker/Config.py
+++ WebChecker/Config.py
@@ -35,17 +35,25 @@ class Config:
                 ))
 
     def writeDeafults(self):
-        self.config.add_section("General")
+        self.config = ConfigParser.ConfigParser(allow_no_value=True)
+        # self.config.add_section("General")
 
         self.config.add_section("Networking")
         self.config.set("Networking", "listen_address", "localhost")
+        self.config.set("Networking", "#Server: bind to this address, use 0.0.0.0 for all interfaces")
+        self.config.set("Networking", "#Client: scheduler's address")
         self.config.set("Networking", "port", "2424")
+        self.config.set("Networking", "#Port to use, have to be same on Server / Client")
         self.config.set("Networking", "receiver_buffer_size", "8192")
+        self.config.set("Networking", "#Buffer size of receiver socket, default should be okay.")
         self.config.set("Networking", "timeout", "30")
+        self.config.set("Networking", "#Sockets' default timeout, default should be okay.")
         self.config.set("Networking", "reconnect_delay", 2) # TODO Change
+        self.config.set("Networking", "#Seconds between retries on client side, when scheduler is gone.")
 
         self.config.add_section("Scheduler")
         self.config.set("Scheduler", "period", "2") #TODO Change to something
+        self.config.set("Scheduler", "#Seconds between scheduler ticks, default should work well. Too low number may cause issues, but should be smaller than the smallest website checking period")
 
         name = "Test Site 1"
         self.config.add_section(name)
