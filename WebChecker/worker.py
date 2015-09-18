57753cfae96ab3a5f3a4affbed0fd4790e3354e8
Peter G. Marczis <peter.marczis@rovio.com>
2015-09-18 10:49:52 +0300

    Some extra network tunning, to get client reconnected, if scheduler lost

--- WebChecker/worker.py
+++ WebChecker/worker.py
@@ -11,24 +11,38 @@ class Worker(asynchat.async_chat):
     def __init__(self):
         asynchat.async_chat.__init__(self)
         self.data = []
-        self.logonce = False
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.logonce = True
         self.scheduler = (Config.get("Networking", "listen_address"), Config.getint("Networking", "port"))
         self.doConnect()
         self.set_terminator("#EOM#")
 
     def doConnect(self):
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         while [ 1 ]:
             try:
                 if self.logonce:
                     logging.info("Try to reconnect: %s:%s" % self.scheduler)
                     self.logonce = False
                 self.connect(self.scheduler)
-                self.logonce = True
-                logging.info("Connected.")
-                return
+
+                if self.connected:
+                    self.logonce = True
+                    logging.info("Connected.")
+                    return
             except:
-                time.sleep(Config.getint("Networking", "reconnect_delay"))
+                pass
+
+            time.sleep(Config.getint("Networking", "reconnect_delay"))
+
+    def handle_connect_event(self):
+        try:
+            asynchat.async_chat.handle_connect_event(self)
+        except socket.error:
+            pass
+
+    def handle_close(self):
+        asynchat.async_chat.handle_close(self)
+        self.doConnect()
 
     def collect_incoming_data(self, data):
         self.data.append(data)
